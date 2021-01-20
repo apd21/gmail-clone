5f60d6b257b66d23e0252d20dbe8e6cab291046d
Apdet <deterding1.a@gmail.com>
2021-01-20 07:59:54 -0600

    cleaned up application by deleting unnecessary files, created header component

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDZZt4wl7lFbkxqK327ZWVKfq1SJ76a8dE",
+    authDomain: "clone-6db23.firebaseapp.com",
+    projectId: "clone-6db23",
+    storageBucket: "clone-6db23.appspot.com",
+    messagingSenderId: "378772945314",
+    appId: "1:378772945314:web:d2d0da499dfed569a0dafb",
+    measurementId: "G-7CZL2X4QXX"
+  };
